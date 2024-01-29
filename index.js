5cce619f2a6fc284fd9cb1c630cdfa5e8ec35208
CoffeerLatte <todty2312@gmail.com>
2024-01-29 12:09:16 +0900

    version 0.3.3 upload

--- index.js
+++ index.js
@@ -37,22 +37,32 @@ console.log(`
 
 `);
 
-const input = process.argv;
+function defaults(a = pa){
+	const  dinput  = a;
+	return dinput;
+}
+
+
+
+const  input = defaults(process.argv);
 
 const s_input = function(process){
-	 
-	return process.slice(2)
+	
+	process.slice(2);
+ 
+	return process.slice(2);
 };
 const r_input = s_input(input)[Math.floor(Math.random()*s_input(input).length)];
 
 class main {
 	constructor(){
 	}
-	call(){
-	console.log("발표자는: "+r_input);
+	call(...index){
+	console.log(index+": "+r_input);
 
 	}
 }
+
 const f = new main();
-f.call();
+f.call("발표자는 ");
 
